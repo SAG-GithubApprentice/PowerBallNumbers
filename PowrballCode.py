0d506a9b33a43672252e009b98abab90edd29a7a
SAG-GithubApprentice <garzon1981@hotmail.com>
2023-12-07 10:53:32 -0700

    Ammended with sorted() to display generated numbers # in sequence.

--- PowrballCode.py
+++ PowrballCode.py
@@ -7,8 +7,9 @@ LuckyNumbers = [random.randint(1, 69) for _ in range(5)]
 # Generate the Powerball number (1 and 26)
 Powerball = random.randint(1, 26)
 
-# Print the generated numbers
-print("Your Lucky 5 Numbers:", LuckyNumbers)
+# Print the generated numbers, (Ammended with sorted() to display generated numbers
+# in sequence.)
+print("Your Lucky 5 Numbers:", sorted(LuckyNumbers))
 print("Your Powerball Number:", Powerball)
 print("Good Luck!")
 
