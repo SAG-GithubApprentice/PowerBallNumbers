38e7c28c687617d85266e6fe1fd663e2fc2f3eae
SAG <148582562+SAG-GithubApprentice@users.noreply.github.com>
2023-12-17 23:27:12 -0700

    Updated PowrballCode.py

--- PowrballCode.py
+++ PowrballCode.py
@@ -2,15 +2,14 @@
 import random
 
 # Generate five random numbers (between 1 and 69)
-LuckyNumbers = [random.randint(1, 69) for _ in range(5)]
+LuckyNumbers = sorted(random.randint(1, 69) for _ in range(5))
 
 # Generate the Powerball number (1 and 26)
 Powerball = random.randint(1, 26)
 
 # Print the generated numbers, (Ammended with sorted() to display generated numbers
 # in sequence.)
-print("Your Lucky 5 Numbers:", sorted(LuckyNumbers))
-print("Your Powerball Number:", Powerball)
-print("Good Luck!")
+print(f"Your 5 lucky numbers are: {LuckyNumbers}")
+print(f"and your Powerball number is {Powerball}. Good luck!")
 
 #Source: https://runestone.academy/ns/books/published/py4e-int/functions/randomnumbers.html
